a6ad563c3de7695dd5fa56cd4f78a4b24ffebfd0
unamorena <92823229+unamorena@users.noreply.github.com>
2022-06-30 13:31:46 +0300

    Refactor train

--- bm/model/train.py
+++ bm/model/train.py
@@ -1,9 +1,8 @@
-# import clearml
 import pandas as pd
 from bm.training.trainer import Trainer
 from bm.training.utils import set_seed
 from bm.config import Config, ProjectPaths
-from bm.data.dataset import BacteriaCombinationsDataset
+from bm.data.dataset import BacteriaCombinationsDataset, BacteriaCombinationsDataset2
 from bm.model.net import SiameseNet
 from torchmetrics import MetricCollection, AUROC, Precision, Recall, Accuracy
 from torch.utils.data import DataLoader
@@ -13,24 +12,20 @@ import albumentations as A
 set_seed()
 torch.cuda.empty_cache()
 
-train_dataset = BacteriaCombinationsDataset(
+train_dataset = BacteriaCombinationsDataset2(
     pd.read_csv(ProjectPaths.data_dir.joinpath('processed/train.csv')),
-    transforms=A.Compose([A.Resize(256, 256), A.Normalize(), A.Flip()]))
-val_dataset = BacteriaCombinationsDataset(
+    transforms=A.Compose([A.Resize(256, 256), A.Normalize()]))
+val_dataset = BacteriaCombinationsDataset2(
     pd.read_csv(ProjectPaths.data_dir.joinpath('processed/val.csv')),
-    transforms=A.Compose([A.Resize(256, 256), A.Normalize(), A.Flip()]))
+    transforms=A.Compose([A.Resize(256, 256), A.Normalize()]))
 train_dataset.combinations_list = train_dataset.combinations_list[::10]
 val_dataset.combinations_list = val_dataset.combinations_list[::]
 
 train_loader, val_loader = DataLoader(train_dataset, batch_size=4, shuffle=True), DataLoader(val_dataset, batch_size=4)
 print('train_dataset', len(train_dataset))
 print('val_dataset', len(val_dataset))
-# print(f'{len(train_dataset)=}')
-# print(f'{len(val_dataset)=}')
 
 model = SiameseNet()
-#for p in model.parameters():
-#    p.requires_grad = False
 
 model.decoder.requires_grad_(True)
 optimizer = torch.optim.AdamW(model.decoder.parameters(), lr=0.00005)
@@ -39,8 +34,7 @@ metrics = MetricCollection({'auroc': AUROC(1),
                             'precision': Precision(1),
                             'recall': Recall(1),
                             'accuracy': Accuracy()})
-# task = clearml.Task.init(Config.project_name, 'test', tags=['test'])
-# logger = task.get_logger()
+
 model_save_path = ProjectPaths.checkpoints_dir.joinpath('test_model')
 
 trainer = Trainer(model,
